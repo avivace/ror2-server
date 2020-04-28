1a93cf5e3ffc01ef428d93c2268a70452f8473b0
Antonio Vivace <avivace4@gmail.com>
2020-04-28 22:23:09 +0200

    Make wine create configuration beforehand

--- entry.sh
+++ entry.sh
@@ -2,4 +2,5 @@
 
 "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir "${STEAMAPPDIR}" +@sSteamCmdForcePlatformType windows +app_update "${STEAMAPPID}" +quit
 
-xvfb-run /opt/wine-stable/bin/wine "${STEAMAPPDIR}/Risk of Rain 2.exe"
+/opt/wine-stable/bin/winecfg
+xvfb-run -a /opt/wine-stable/bin/wine "${STEAMAPPDIR}/Risk of Rain 2.exe"
