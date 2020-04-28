a4bd644c396711a17945cdd4790008bbc27f50fc
Davide Casella <dcasella@libc.it>
2020-04-28 22:27:45 +0200

    Remove -a

--- entry.sh
+++ entry.sh
@@ -3,4 +3,4 @@
 "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir "${STEAMAPPDIR}" +@sSteamCmdForcePlatformType windows +app_update "${STEAMAPPID}" +quit
 
 /opt/wine-stable/bin/winecfg
-xvfb-run -a /opt/wine-stable/bin/wine "${STEAMAPPDIR}/Risk of Rain 2.exe"
+xvfb-run /opt/wine-stable/bin/wine "${STEAMAPPDIR}/Risk of Rain 2.exe"
