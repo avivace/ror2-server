29ffd9de9c57a91902bcd2db433dbbc0f9dc3af6
Antonio Vivace <avivace4@gmail.com>
2020-04-28 22:48:55 +0200

    winecfg is broken as hell

--- entry.sh
+++ entry.sh
@@ -3,4 +3,7 @@
 "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir "${STEAMAPPDIR}" +@sSteamCmdForcePlatformType windows +app_update "${STEAMAPPID}" +quit
 
 /opt/wine-stable/bin/winecfg
+echo "Let's wait :)"
+sleep 5
+echo "ecoci qui"
 xvfb-run /opt/wine-stable/bin/wine "${STEAMAPPDIR}/Risk of Rain 2.exe"
