3c63044be3bf33ee7e513d2f965273602c1efc31
Antonio Vivace <avivace4@gmail.com>
2020-04-29 01:58:29 +0200

    More echoes

--- entry.sh
+++ entry.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+echo "Updating Risk of Rain 2 server..."
 "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir "${STEAMAPPDIR}" +@sSteamCmdForcePlatformType windows +app_update "${STEAMAPPID}" +quit
 
+echo "Generating initial wine configuration..."
 /opt/wine-stable/bin/winecfg
+
 echo "Let's wait :)"
 sleep 5
-echo "ecoci qui"
+
+echo "Starting server..."
 xvfb-run /opt/wine-stable/bin/wine "${STEAMAPPDIR}/Risk of Rain 2.exe"
