3e775989c9cbac38860c59472fc018e85b2a3b2c
Antonio Vivace <avivace4@gmail.com>
2020-04-29 12:26:11 +0200

    Generate configuration file from template and environment variables

--- entry.sh
+++ entry.sh
@@ -3,7 +3,10 @@
 echo "Updating Risk of Rain 2 server..."
 "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir "${STEAMAPPDIR}" +@sSteamCmdForcePlatformType windows +app_update "${STEAMAPPID}" +quit
 
-echo "Generating initial wine configuration..."
+echo "Generating server configuration..."
+envsubst < "default_config.cfg" > "${STEAMAPPDIR}/Risk of Rain 2_Data/Config/server.cfg"
+
+echo "Generating initial Wine configuration..."
 /opt/wine-stable/bin/winecfg
 
 echo "Let's wait :)"
