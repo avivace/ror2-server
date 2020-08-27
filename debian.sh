22d39474466d6204f09629e2a4dc3a31e626bbc7
Antonio Vivace <avivace4@gmail.com>
2020-08-27 13:51:17 +0200

    Add Docker’s repository GPG key

--- debian.sh
+++ debian.sh
@@ -7,6 +7,7 @@ apt-get install \
     curl \
     gnupg-agent \
     software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
