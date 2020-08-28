61890927435038b76825d3084645049d8a182e33
Manuel Rota <manuel.rota@protonmail.ch>
2020-08-28 23:33:24 +0200

    add support for mods and R2_ENABLE_MODS flag

--- entry.sh
+++ entry.sh
@@ -6,6 +6,14 @@ echo "Updating Risk of Rain 2 server..."
 echo "Generating server configuration..."
 envsubst < "default_config.cfg" > "${STEAMAPPDIR}/Risk of Rain 2_Data/Config/server.cfg"
 
+if [ "${R2_ENABLE_MODS}" = true ]; then
+    echo "Setup mods..."
+    cp -r ${STEAMAPPDIR}/mods/BepInEx             ${STEAMAPPDIR}/BepInEx
+    cp    ${STEAMAPPDIR}/mods/doorstop_config.ini ${STEAMAPPDIR}/doorstop_config.ini
+    cp    ${STEAMAPPDIR}/mods/winhttp.dll         ${STEAMAPPDIR}/winhttp.dll
+    DLL="winhttp=n,b"
+fi
+
 echo "Generating initial Wine configuration..."
 /opt/wine-stable/bin/winecfg
 
@@ -13,4 +21,4 @@ echo "Let's wait :)"
 sleep 5
 
 echo "Starting server..."
-xvfb-run /opt/wine-stable/bin/wine "${STEAMAPPDIR}/Risk of Rain 2.exe"
+WINEDLLOVERRIDES=${DLL} xvfb-run /opt/wine-stable/bin/wine "${STEAMAPPDIR}/Risk of Rain 2.exe"
