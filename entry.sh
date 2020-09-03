10e3f59513d6a3f69e180699415344f39048dc4b
Antonio Vivace <avivace4@gmail.com>
2020-09-03 16:55:45 +0200

    Now enabling mod support setting `R2_ENABLE_MODS` to `1` (from `true`)

--- entry.sh
+++ entry.sh
@@ -6,8 +6,8 @@ echo "Updating Risk of Rain 2 server..."
 echo "Generating server configuration..."
 envsubst < "default_config.cfg" > "${STEAMAPPDIR}/Risk of Rain 2_Data/Config/server.cfg"
 
-if [ "${R2_ENABLE_MODS}" = true ]; then
-    echo "Setup mods..."
+if [ "${R2_ENABLE_MODS}" = 1 ]; then
+    echo "Setting up mods..."
     cp -r ${STEAMAPPDIR}/mods/BepInEx             ${STEAMAPPDIR}/BepInEx
     cp    ${STEAMAPPDIR}/mods/doorstop_config.ini ${STEAMAPPDIR}/doorstop_config.ini
     cp    ${STEAMAPPDIR}/mods/winhttp.dll         ${STEAMAPPDIR}/winhttp.dll
