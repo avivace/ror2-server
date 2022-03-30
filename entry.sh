35533224ab4fef2132bfe4ebd03972a0b5acd64b
Manuel Rota <manuel.rota@protonmail.ch>
2022-03-30 22:55:11 +0200

    Add new dockerfiles for vanilla, wine6 and wine7 versions, all based on steamcmd/steamcmd:ubuntu-20

--- entry.sh
+++ entry.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 echo "Installing Risk of Rain 2 server..."
-"${STEAMCMDDIR}/steamcmd.sh" +force_install_dir "${STEAMAPPDIR}" +login anonymous +@sSteamCmdForcePlatformType windows +app_update "${STEAMAPPID}" +quit
+"${STEAMCMD}" +force_install_dir "${STEAMAPPDIR}" +login anonymous +@sSteamCmdForcePlatformType windows +app_update "${STEAMAPPID}" +quit
 
 echo "Generating server configuration..."
 envsubst < "default_config.cfg" > "${STEAMAPPDIR}/Risk of Rain 2_Data/Config/server.cfg"
 
 if [ "${R2_ENABLE_MODS}" = 1 ]; then
     echo "Setting up mods..."
-    rm -rf ${STEAMAPPDIR}/BepInEx
-    cp -r  ${MODDIR}/BepInEx             ${STEAMAPPDIR}/BepInEx
-    cp     ${MODDIR}/doorstop_config.ini ${STEAMAPPDIR}/doorstop_config.ini
-    cp     ${MODDIR}/winhttp.dll         ${STEAMAPPDIR}/winhttp.dll
+    rm -rf "${STEAMAPPDIR}/BepInEx"
+    cp -r  "${MODDIR}/BepInEx"             "${STEAMAPPDIR}/BepInEx"
+    cp     "${MODDIR}/doorstop_config.ini" "${STEAMAPPDIR}/doorstop_config.ini"
+    cp     "${MODDIR}/winhttp.dll"         "${STEAMAPPDIR}/winhttp.dll"
     DLL="winhttp=n,b"
 fi
 
 echo "Generating initial Wine configuration..."
-/opt/wine-stable/bin/winecfg
+winecfg
 
 echo "Let's wait :)"
 sleep 5
 
 echo "Starting server..."
-WINEDLLOVERRIDES=${DLL} xvfb-run /opt/wine-stable/bin/wine "${STEAMAPPDIR}/Risk of Rain 2.exe"
+WINEDLLOVERRIDES=${DLL} xvfb-run wine "${STEAMAPPDIR}/Risk of Rain 2.exe"
