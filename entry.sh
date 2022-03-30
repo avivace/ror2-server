24f6730499d0693e91281b427d78d85deaaf0d82
Manuel Rota <manuel.rota@protonmail.ch>
2022-03-30 23:16:40 +0200

    Add HEALTHCHECK for server readiness

--- entry.sh
+++ entry.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-echo "Installing Risk of Rain 2 server..."
-"${STEAMCMD}" +force_install_dir "${STEAMAPPDIR}" +login anonymous +@sSteamCmdForcePlatformType windows +app_update "${STEAMAPPID}" +quit
+function execute()
+{
+    echo "Installing Risk of Rain 2 server..."
+    "${STEAMCMD}" +force_install_dir "${STEAMAPPDIR}" +login anonymous +@sSteamCmdForcePlatformType windows +app_update "${STEAMAPPID}" +quit
 
-echo "Generating server configuration..."
-envsubst < "default_config.cfg" > "${STEAMAPPDIR}/Risk of Rain 2_Data/Config/server.cfg"
+    echo "Generating server configuration..."
+    envsubst < "default_config.cfg" > "${STEAMAPPDIR}/Risk of Rain 2_Data/Config/server.cfg"
 
-if [ "${R2_ENABLE_MODS}" = 1 ]; then
-    echo "Setting up mods..."
-    rm -rf "${STEAMAPPDIR}/BepInEx"
-    cp -r  "${MODDIR}/BepInEx"             "${STEAMAPPDIR}/BepInEx"
-    cp     "${MODDIR}/doorstop_config.ini" "${STEAMAPPDIR}/doorstop_config.ini"
-    cp     "${MODDIR}/winhttp.dll"         "${STEAMAPPDIR}/winhttp.dll"
-    DLL="winhttp=n,b"
-fi
+    if [ "${R2_ENABLE_MODS}" = 1 ]; then
+        echo "Setting up mods..."
+        rm -rf "${STEAMAPPDIR}/BepInEx"
+        cp -r  "${MODDIR}/BepInEx"             "${STEAMAPPDIR}/BepInEx"
+        cp     "${MODDIR}/doorstop_config.ini" "${STEAMAPPDIR}/doorstop_config.ini"
+        cp     "${MODDIR}/winhttp.dll"         "${STEAMAPPDIR}/winhttp.dll"
+        DLL="winhttp=n,b"
+    fi
 
-echo "Generating initial Wine configuration..."
-winecfg
+    echo "Generating initial Wine configuration..."
+    winecfg
 
-echo "Let's wait :)"
-sleep 5
+    echo "Let's wait :)"
+    sleep 5
 
-echo "Starting server..."
-WINEDLLOVERRIDES=${DLL} xvfb-run wine "${STEAMAPPDIR}/Risk of Rain 2.exe"
+    echo "Starting server..."
+    WINEDLLOVERRIDES=${DLL} xvfb-run wine "${STEAMAPPDIR}/Risk of Rain 2.exe"
+}
+
+# Redirect both stdout and stderr to stdout and to a file
+execute 2>&1 | tee "${STEAMAPPDIR}/entry.log"
